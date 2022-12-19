d17e2a69fa8e25066c8834909ed9ea124bc9cf94
Nathan Hau <github@nathanhau.anonaddy.com>
2022-12-19 23:56:35 +0800

    changed optuna.py to tune.py

--- tune.py
+++ tune.py
@@ -1,5 +1,5 @@
 import optuna
-# from optuna.trial import TrialState
+from optuna.trial import TrialState
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,29 +12,34 @@ from models.SGC import SGC
 from stacked_models import DeepLinear, LinearMLP
 from models.GCN import GCN
 from dgl.nn.pytorch.conv import SGConv
+from time import perf_counter
 
 
 
 
-graph, label, split_idx = load_data('cora')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-graph = preprocess_graph(graph).to(device)
-label=label.to(device)
-  # print(graph.ndata['feat'])
-raw_features = graph.ndata['feat'].to(device)
 
-in_feats = raw_features.shape[-1]
-n_classes = label.max().item()+1
-K=2
-
-model=SGC(in_feats,n_classes,K,device)
-epochs=100
-train_mask,val_mask=split_idx["train_mask"], split_idx["val_mask"]
 
 def objective(trial):
-    lr=trial.sugget_float("lr",1e-5,1e-3)
-    weight_decay=trial.sugget_float('weight_decay',1e-7,1e-3)
+    graph, labels, split_idx = load_data('cora')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    g = preprocess_graph(graph).to(device)
+    labels=labels.to(device)
+    # print(graph.ndata['feat'])
+    features = graph.ndata['feat'].to(device)
+
+    in_feats = features.shape[-1]
+    n_classes = labels.max().item()+1
+    K=2
+    is_linear=False
+    loss_fn=nn.CrossEntropyLoss()
+    model=SGC(in_feats,n_classes,K,device)
+    epochs=100
+    train_mask,val_mask=split_idx["train_mask"], split_idx["val_mask"]
+
+    lr=trial.suggest_float("lr",1e-5,1e-3)
+    weight_decay=trial.suggest_float('weight_decay',1e-7,1e-3)
     
     t = perf_counter()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
@@ -63,23 +68,23 @@ def objective(trial):
         train_acc = torch.sum(logits[train_mask].argmax(1) == labels[train_mask]).item() / train_mask.sum().item()
         val_acc = torch.sum(logits[val_mask].argmax(1) == labels[val_mask]).item() / val_mask.sum().item()
         # test_acc = torch.sum(logits[test_mask].argmax(1) == labels[test_mask]).item() / test_mask.sum().item()
-        print(f'Epoch {epoch + 1:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, Val: {val_acc:.4f}')
+        # print(f'Epoch {epoch + 1:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, Val: {val_acc:.4f}')
         trial.report(val_acc,epoch)
     training_time = perf_counter()-t
-    # print(f'Training time: {training_time:.4f}s')
+    print(f'Training time: {training_time:.4f}s')
     return val_acc
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
     study.optimize(objective, n_trials=100, timeout=600)
 
-    # pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-    # complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
 
-    # print("Study statistics: ")
-    # print("  Number of finished trials: ", len(study.trials))
-    # print("  Number of pruned trials: ", len(pruned_trials))
-    # print("  Number of complete trials: ", len(complete_trials))
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
 
     print("Best trial:")
     trial = study.best_trial
